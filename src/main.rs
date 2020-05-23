5b6450e7fc3d127ea22c5df22bd152f73f3c06c3
Phil <PhilboBaggins@users.noreply.github.com>
2020-05-24 08:19:00 +1000

    Replace cargo-init's main.rs code with serenity's getting started example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate serenity;
+
+use serenity::client::{Client, EventHandler};
+use serenity::framework::standard::StandardFramework;
+use std::env;
+
+struct Handler;
+
+impl EventHandler for Handler {}
+
+pub fn main() {
+    // Login with a bot token from the environment
+    let mut client = Client::new(&env::var("DISCORD_TOKEN").expect("token"), Handler)
+        .expect("Error creating client");
+
+    client.with_framework(StandardFramework::new()
+        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
+        .cmd("ping", ping));
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start() {
+        println!("An error occurred while running the client: {:?}", why);
+    }
 }
+
+command!(ping(_context, message) {
+    let _ = message.reply("Pong!");
+});
