b7465975070c37defd6fd406213106ffc67c418e
Phil <PhilboBaggins@users.noreply.github.com>
2020-05-24 09:11:52 +1000

    Respond to any message that mentions the bot, rather than just commands

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,39 @@
-#[macro_use] extern crate serenity;
-
-use serenity::client::{Client, EventHandler};
-use serenity::framework::standard::StandardFramework;
-
-use std::env;
+use serenity::{
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+};
 
 use rand::thread_rng;
 use rand::seq::SliceRandom;
 
+const MY_CLIENT_ID: u64 = 713872191682510909;
+
 struct Handler;
 
-impl EventHandler for Handler {}
+impl EventHandler for Handler {
+    fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+
+    fn message(&self, _ctx: Context, msg: Message) {
+        if msg.mentions_user_id(MY_CLIENT_ID) {
+            let choices = ["red", "blue"];
+            let mut rng = thread_rng();
+            let choice = choices.choose(&mut rng).unwrap();
+            if let Err(why) = msg.channel_id.say(choice) {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+    }
+}
 
 pub fn main() {
     // Login with a bot token from the environment
     let mut client = Client::new(&env::var("DISCORD_TOKEN").expect("token"), Handler)
         .expect("Error creating client");
 
-    client.with_framework(StandardFramework::new()
-        .configure(|c| c.prefix("")) // set the bot's prefix to "~"
-        .cmd("?", red_or_blue));
-
     // start listening for events by starting a single shard
     if let Err(why) = client.start() {
         println!("An error occurred while running the client: {:?}", why);
     }
 }
-
-command!(red_or_blue(_context, message) {
-    let choices = ["red", "blue"];
-    let mut rng = thread_rng();
-    let choice = choices.choose(&mut rng).unwrap();
-    let _ = message.reply(choice);
-});
