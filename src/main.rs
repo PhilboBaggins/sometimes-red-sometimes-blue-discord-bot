47e9afc85a42d9dfc8da80f8341846e7039b0aa7
Phil <PhilboBaggins@users.noreply.github.com>
2020-05-25 03:04:45 +1000

    Fix formatting

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use serenity::{
-    model::{channel::Message, gateway::Ready},
     builder::CreateMessage,
-    utils::Colour,
+    model::{channel::Message, gateway::Ready},
     prelude::*,
+    utils::Colour,
 };
 
-use rand::thread_rng;
 use rand::seq::SliceRandom;
+use rand::thread_rng;
 
+use std::fmt;
+
+// TODO: Don't hard-code this, read it dynamicially ... perhaps with serenity::http::raw::get_current_user()
 const MY_CLIENT_ID: u64 = 713872191682510909;
 
 struct Handler;
@@ -15,13 +18,13 @@ struct Handler;
 // TODO: Consider using rand_derive
 enum Colours {
     Red,
-    Blue
+    Blue,
 }
 
 impl fmt::Display for Colours {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Colours::Red => write!(f, "{}","Red"),
+            Colours::Red => write!(f, "{}", "Red"),
             Colours::Blue => write!(f, "{}", "Blue"),
         }
     }
@@ -40,11 +43,7 @@ fn gen_colour_message(message: CreateMessage) -> CreateMessage {
     let choices = [Colours::Red, Colours::Blue];
     let mut rng = thread_rng();
     let choice = choices.choose(&mut rng).unwrap();
-    message
-        //.content(choice)
-        .embed(|e| e
-            .title(choice)
-            .colour(choice.to_colour()))
+    message.embed(|e| e.title(choice).colour(choice.to_colour()))
 }
 
 impl EventHandler for Handler {
@@ -66,7 +65,7 @@ pub fn main() {
     let mut client = Client::new(&env::var("DISCORD_TOKEN").expect("token"), Handler)
         .expect("Error creating client");
 
-    // start listening for events by starting a single shard
+    // Start listening for events by starting a single shard
     if let Err(why) = client.start() {
         println!("An error occurred while running the client: {:?}", why);
     }
