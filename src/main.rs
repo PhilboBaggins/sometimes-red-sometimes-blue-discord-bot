f1721ad76f549f93ce27428eb45ddd34ab52e689
Phil <PhilboBaggins@users.noreply.github.com>
2020-05-24 20:34:36 +1000

    Send messages with embedded colours

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use serenity::{
     model::{channel::Message, gateway::Ready},
+    builder::CreateMessage,
+    utils::Colour,
     prelude::*,
 };
 
@@ -10,6 +12,39 @@ const MY_CLIENT_ID: u64 = 713872191682510909;
 
 struct Handler;
 
+// TODO: Consider using rand_derive
+enum Colours {
+    Red,
+    Blue
+}
+
+impl Colours {
+    fn to_string(&self) -> &'static str {
+        match self {
+            Colours::Red => "Red",
+            Colours::Blue => "Blue",
+        }
+    }
+
+    fn to_colour(&self) -> Colour {
+        match self {
+            Colours::Red => Colour::from_rgb(255, 0, 0),
+            Colours::Blue => Colour::from_rgb(0, 0, 255),
+        }
+    }
+}
+
+fn gen_colour_message(message: CreateMessage) -> CreateMessage {
+    let choices = [Colours::Red, Colours::Blue];
+    let mut rng = thread_rng();
+    let choice = choices.choose(&mut rng).unwrap();
+    message
+        //.content(choice.to_string())
+        .embed(|e| e
+            .title(choice.to_string())
+            .colour(choice.to_colour()))
+}
+
 impl EventHandler for Handler {
     fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
@@ -17,10 +52,7 @@ impl EventHandler for Handler {
 
     fn message(&self, _ctx: Context, msg: Message) {
         if !msg.is_own() && (msg.is_private() || msg.mentions_user_id(MY_CLIENT_ID)) {
-            let choices = ["red", "blue"];
-            let mut rng = thread_rng();
-            let choice = choices.choose(&mut rng).unwrap();
-            if let Err(why) = msg.channel_id.say(choice) {
+            if let Err(why) = msg.channel_id.send_message(gen_colour_message) {
                 println!("Error sending message: {:?}", why);
             }
         }
